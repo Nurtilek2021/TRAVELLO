68803b0745ec1f6db23fc73b1c001f2fa19895f2
Nurtilek2021 <kaldarovnurtilek13@gmail.com>
2021-11-05 08:42:08 +0600

    'TRAVELLO'

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items: 3,
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+    });
+});
+
+$(document).ready(function(){
+    $(".carousel1").owlCarousel({
+        items: 5,
+        margin: 23,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+    });
+});
